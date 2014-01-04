856ebfae28958d19c22846eacac647186cca5e2e
Oleh Krehel <ohwoeowho@gmail.com>
2014-01-04 15:12:50 +0100

    org-download.el: update licence

--- org-download.el
+++ org-download.el
@@ -1,25 +1,25 @@
 ;;; org-download.el --- Image drag-and-drop for Emacs org-mode
 
-;; Copyright (C) 2013  Oleh Krehel
+;; Copyright (C) 2014 Free Software Foundation, Inc.
 
-;; Author: Oleh Krehel <ohwoeowho@gmail.com>
-;; URL: https://github.com/abo-abo/org-download
-;; Version: 0.1
+;; Author: Oleh Krehel
+;; Keywords: images, screenshots, download
+;; Homepage: http://orgmode.org
 
-;; This file is not part of GNU Emacs
+;; This file is part of GNU Emacs.
 
-;; This file is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
